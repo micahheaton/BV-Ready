ab86e231f610de096edaf4b180a51ccf6a2dda80
micahheaton <123677910+micahheaton@users.noreply.github.com>
2024-04-08 21:08:09 -0700

    adjust app.py

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 import streamlit as st
-
-# Import your different applications
-import TGAreport_generator
-import report_generator
-import sentinel_report_generator
+import subprocess
 
 def main():
     st.set_page_config(page_title="Report Generators", page_icon=None, layout="wide", initial_sidebar_state="auto")
+    
     st.title("Report Generators")
+    
     st.write("Select a report generator to run:")
-
-    option = st.radio("Select an option", ("M365 Threat Gap Analysis Report Generator", "Report Generator", "Sentinel Report Generator"))
-
-    if option == "M365 Threat Gap Analysis Report Generator":
-        TGAreport_generator.app()
-    elif option == "Report Generator":
-        report_generator.app()
-    elif option == "Sentinel Report Generator":
-        sentinel_report_generator.app()
+    
+    if st.button("M365 Threat Gap Analysis Report Generator"):
+        subprocess.Popen(["streamlit", "run", "TGAreport_generator.py"])
+    
+    if st.button("Report Generator"):
+        subprocess.Popen(["streamlit", "run", "report_generator.py"])
+    
+    if st.button("Sentinel Report Generator"):
+        subprocess.Popen(["streamlit", "run", "sentinel_report_generator.py"])
 
 if __name__ == "__main__":
     main()
