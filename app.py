e2b7da8a23036ecc482cb55bfc90811df77a2773
micahheaton <123677910+micahheaton@users.noreply.github.com>
2024-04-08 21:27:51 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 # Import your different applications
 import TGAreport_generator
-import report_generator
-import sentinel_report_generator
 
 def main():
     st.set_page_config(page_title="Report Generators", page_icon=None, layout="wide", initial_sidebar_state="auto")
@@ -14,10 +12,6 @@ def main():
 
     if option == "M365 Threat Gap Analysis Report Generator":
         TGAreport_generator.app()
-    elif option == "Report Generator":
-        report_generator.app()
-    elif option == "Sentinel Report Generator":
-        sentinel_report_generator.app()
 
 if __name__ == "__main__":
     main()
